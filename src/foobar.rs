f8b84eec33ed1e9f5724966fd4f54d24b2d12785
Your Name <you@example.com>
2022-07-01 14:39:37 +0800

    add unit test

--- src/foobar.rs
+++ src/foobar.rs
@@ -1,3 +1,19 @@
-pub fn foobar(){
-  println!("foobar");
+pub fn foobar() {
+    println!("foobar");
+}
+
+pub fn add(x: i32, y: i32) -> i32{
+    let result = x + y;
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::foobar::add;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
 }
